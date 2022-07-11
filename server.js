592b4f84d981a099f573e6d11ef092c62d2b1b26
Ryan Reeves <reeves0ryan@gmail.com>
2022-07-11 01:07:30 -0700

    Fixed heroku deployment bug

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const api = require('./routes/api');
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 
